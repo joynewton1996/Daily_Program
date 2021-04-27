1fc07e710995af0b1e559a18b8c063c6af499351
Joy_Newton <joynewton1964@gmail.com>
2021-04-27 11:02:49 +0530

    luckey number

--- def_function.py
+++ def_function.py
@@ -1,11 +1,20 @@
 # Seconds convert
 
 def seconds_convert(seconds):
-    hours = (seconds) // 3600
-    minute = (seconds - (hours * 3600)) // 60
-    remaining_seconds = (seconds - (hours * 3600) - (minute * 60))
-    return (hours, minute, remaining_seconds)
+    hour = seconds // 3600
+    minute = (seconds - (hour * 3600)) // 60
+    second = (seconds - (hour * 3600) - (minute * 60))
+    return hour, minute, second
+
 
 hours, minute, remaining_seconds = seconds_convert(5000)
 
-print(hours,minute,remaining_seconds)
+print(hours, minute, remaining_seconds)
+
+
+def luckey_number(name):
+    number = len(name) * 9
+    print("the luckey number " + name + " is : " + str(number))
+
+
+luckey_number("hello")
