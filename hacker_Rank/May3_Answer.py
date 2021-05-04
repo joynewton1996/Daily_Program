9f37fa2fb2a830d770b4ab30e89dbbffc45818bd
Joy_Newton <joynewton1964@gmail.com>
2021-05-04 22:26:52 +0530

    long gap updated

--- hacker_Rank/May3_Answer.py
+++ hacker_Rank/May3_Answer.py
@@ -1,28 +1,32 @@
 def solution(n):
     count = 0
     binary_list = []
+    long_gap = 0
     if n % 2 != 0 and n % 4 != 0:
         while n > 0:
             r = n % 2
             binary_list.append(r)
             n = n // 2
         binary_list.reverse()
-        # print(binary_list)
+        print(binary_list)
         if binary_list[0] == 1:
             for i in range(1, len(binary_list)):
                 if binary_list[i] == 0:
                     count += 1
-                else:
-                    break
-    else:
-        return print(count)
+                elif long_gap < count:
+                    long_gap = count
+                    count = 0
+        return print(long_gap)
 
-    print(count)
+
+    else:
+
+        return print(long_gap)
+
 
 
 n = int(input())
 solution(n)
-
 
 # n = 5
 # binary = []
@@ -49,3 +53,11 @@ solution(n)
 # else:
 #     print(count)
 
+# def decimal_binary(start,end):
+#     for i in range(start,end+1):
+#         while i > 0:
+#             temp =
+#
+#
+#
+# decimal_binary(1,10)
