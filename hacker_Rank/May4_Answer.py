84816f8c35b8a6501e7e8b24d391d44cf209e8c7
Joy_Newton <joynewton1964@gmail.com>
2021-05-06 12:54:05 +0530

    removed unwanted codes

--- hacker_Rank/May4_Answer.py
+++ hacker_Rank/May4_Answer.py
@@ -1,33 +1,17 @@
 def array_rotation(a, k):
-    result = []
     print(a)
-    step = 1
     for i in range(k):
         step = 1
-        print("for_loop:" + str(i), a)
         while step < len(a):
             temp = a[0]
-            a[0] = a[(0+step)]
-            a[(0+step)] = temp
-            print("while_loop:" + str(step) , a)
+            a[0] = a[(0 + step)]
+            a[(0 + step)] = temp
             step += 1
-
-        # # while step < len(a)-2:
-        #     # print(a)
-        #     step_temp = a[0]
-        #     a[0] = a[step + 2]
-        #     a[step + 2] = step_temp
-        #     step += 1
-        #     print("loop" ,a)
-
-    print("Result_list", a)
-
-
+    print("Result", a)
 
 
 # a = [3,8,9,7,6]
-a = [1,2,3,4,5]
-k = 1
-
+a = [1, 2, 3, 4, 5]
+k = 2
 
 array_rotation(a, k)
