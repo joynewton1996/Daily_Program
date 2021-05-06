4593e52b7cc588ce5dd6302df19a774cbea3add3
Joy_Newton <joynewton1964@gmail.com>
2021-05-06 12:23:57 +0530

    some reduced format

--- hacker_Rank/May4_Answer.py
+++ hacker_Rank/May4_Answer.py
@@ -1,31 +1,33 @@
 def array_rotation(a, k):
     result = []
     print(a)
-    step = 0
+    step = 1
     for i in range(k):
-        print("ref", a)
-        print(i)
-        temp = a[0]
-        a[0] = a[(0+1)]
-        a[(0+1)] = temp
-        print("fist_loop:", a)
-        step = 0
-
-        while step < len(a)-2:
-            # print(a)
-            step_temp = a[0]
-            a[0] = a[step + 2]
-            a[step + 2] = step_temp
+        step = 1
+        print("for_loop:" + str(i), a)
+        while step < len(a):
+            temp = a[0]
+            a[0] = a[(0+step)]
+            a[(0+step)] = temp
+            print("while_loop:" + str(step) , a)
             step += 1
-            print("loop" ,a)
 
-    print("final", a)
+        # # while step < len(a)-2:
+        #     # print(a)
+        #     step_temp = a[0]
+        #     a[0] = a[step + 2]
+        #     a[step + 2] = step_temp
+        #     step += 1
+        #     print("loop" ,a)
+
+    print("Result_list", a)
 
 
 
 
-a = [3,8,9,7,6]
-k = 3
+# a = [3,8,9,7,6]
+a = [1,2,3,4,5]
+k = 1
 
 
 array_rotation(a, k)
