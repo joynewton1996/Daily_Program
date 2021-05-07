f473178be63020a91d720031af7767a8ccea6535
Joy_Newton <joynewton1964@gmail.com>
2021-05-07 13:37:10 +0530

    return -1 updated

--- hacker_Rank/May5_Answer.py
+++ hacker_Rank/May5_Answer.py
@@ -1,20 +1,22 @@
 def earliest_time(a, x):
     print(a)
     b = []
+
     for i in range(len(a)):
-        if 1 <= a[i] <= x:
+        if a[i] == x or a[i] <= x:
             for j in range(len(a)):
                 if a[i] == a[j] and a[i] not in b:
                     b.append(a[i])
                     seconds = j
-
-    print("fianl", b)
-    return "seconds " + str(seconds) + "  " + "Number of steps:" + str(b)
+    if x in b:
+        return "seconds " + str(seconds) + "  " + "Number of steps:" + str(b)
+    else:
+        return -1
 
 
 if __name__ == '__main__':
     # Call your function here
     a = [1, 3, 1, 4, 2, 3, 5, 4]
-    x = 2
+    x = 3
     time = earliest_time(a, x)
     print(time)
