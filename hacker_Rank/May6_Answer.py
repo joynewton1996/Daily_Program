885a682c2361ad805cfa56e3f57dc789d5076690
Joy_Newton <joynewton1964@gmail.com>
2021-05-09 00:34:43 +0530

    return added

--- hacker_Rank/May6_Answer.py
+++ hacker_Rank/May6_Answer.py
@@ -16,10 +16,13 @@ def solution(a):
         if b[step] != b[step + 1]:
             return b[step]
         step += 2
+    return b[step]
+
 
 
 if __name__ == '__main__':
     # Call the fumction
-    a = [9, 3, 9, 3, 9, 7, 9]
+    # a = [9, 3, 9, 3, 9, 7, 9]
+    a = [3,3,1,3,1]
     print(solution(a))
     # print(sort(a))
