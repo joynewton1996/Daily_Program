574b7d9ab92c1c93a10c61d4f2c4a28009d08cdf
Joy_Newton <joynewton1964@gmail.com>
2021-05-12 01:04:16 +0530

    set used

--- hacker_Rank/May8_Answer.py
+++ hacker_Rank/May8_Answer.py
@@ -6,32 +6,21 @@ obj = Algo()
 def solution(a):
     if a >= [0]:
         Max_value = obj.max_value(a)
-        sequence = sequence_generator(Max_value)
-        result = []
-        for value in sequence:
-            if value not in a:
-                result.append(value)
+        sequence = {item for item in range(1, Max_value + 2)}
+        a = set(a)
+        print(a)
+        difference = sequence.difference(a)
+        difference = list(difference)
+        return difference[0]
 
-        if result > []:
-            result = obj.sort(result)
-            return result
-        else:
-            result.append(Max_value + 1)
-            return result
     else:
-        return [1]
-
-
-def sequence_generator(n):
-    sequence = []
-    step = 1
-    while step <= n:
-        sequence.append(step)
-        step += 1
-    return sequence
+        return 1
 
 
 if __name__ == '__main__':
     a = [6, 3, 1, 4, 1, 2]
+    b = [-1, -3]
+    c = [1, 3, 6, 4, 5]
+    d = [1, 2, 3, 4]
     seq = solution(a)
     print(seq)
