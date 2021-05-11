1b61eda08163bea5822f556fb2386e60e75f9b85
Joy_Newton <joynewton1964@gmail.com>
2021-05-11 11:55:39 +0530

    removed unwanted print statement

--- hacker_Rank/Mocktest_may9 _long_Gap.py
+++ hacker_Rank/Mocktest_may9 _long_Gap.py
@@ -5,23 +5,15 @@ def long_gap_character_in_string(s):
     letter = ""
     print(a)
     for i in range(len(a)):
-        # print("iteration i ", i)
         for j in range(i + 1, len(a)):
-            # print("iteration J ", j)
-            # print(a[i], "==", a[j])
             if a[i] == a[j]:
-                # print("if", a[i], "==", a[j])
                 count += (j - i)
-                # print("if count", count)
                 count = count - long_gap
             elif long_gap <= count:
-
-                # print("long_gap", long_gap, "<", count)
                 long_gap = count
-                # print("long_gap", long_gap)
                 letter = a[i]
                 count = 0
-    print(letter, "long gap", long_gap)
+    print(str("'")+letter+"'", "long gap", long_gap)
 
 
 if __name__ == '__main__':
