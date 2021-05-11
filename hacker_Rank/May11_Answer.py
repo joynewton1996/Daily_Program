59f2968a1737817fa254c74ac5625e264a7dfe70
Joy_Newton <joynewton1964@gmail.com>
2021-05-12 01:14:41 +0530

    set used in program

--- hacker_Rank/May11_Answer.py
+++ hacker_Rank/May11_Answer.py
@@ -4,31 +4,18 @@ obj = Algo()
 
 
 def solutuion(a):
-    print(a)
     max_value = obj.max_value(a)
-    factorial = factorial_list(max_value)
-    for item in factorial:
-        if item not in a:
-            return 0
-        else:
-            return 1
-
-
-def factorial_list(n):
-    factorial = []
-    step = 0
-    while step < n and n > 1:
-        k = (n - step)
-        step += 1
-        factorial.append(k)
-
-    if n == 1:
-        return factorial[1]
-
-    return factorial
+    series = set(item for item in range(1, max_value + 1))
+    a = set(a)
+    difference = series.difference(a)
+    if difference == 0:
+        return 1
+    else:
+        return 0
 
 
 if __name__ == "__main__":
     a = [4, 1, 3, 2]
-    permutation = solutuion(a)
+    b=[1]
+    permutation = solutuion(b)
     print(permutation)
