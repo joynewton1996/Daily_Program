986334273b2c7e641f7a31f944c2ec4247006b31
Joy_Newton <joynewton1964@gmail.com>
2021-05-12 13:09:30 +0530

    min value added

--- algorithm.py
+++ algorithm.py
@@ -12,7 +12,7 @@ class Algo:
 
     def max_value(self,x):
         # print(x)
-        for i in range(len(x) - 1):
+        for i in range(len(x)-1):
             if x[i] > x[i + 1]:
                 temp = x[i]
                 x[i] = x[i + 1]
@@ -28,6 +28,19 @@ class Algo:
             bin_number.append(r)
         bin_number.reverse()
         return bin_number
+
+    def min_value(self,a , maxvalue = False):
+        a = list(a)
+        for j in range(len(a) - 1):
+            for i in range(len(a) - 1):
+                if a[i] > a[i + 1]:
+                    temp = a[i]
+                    a[i] = a[i + 1]
+                    a[i + 1] = temp
+        if maxvalue :
+            return a[-1]
+        else:
+            return a[0]
 
 
 
