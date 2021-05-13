168106eec05b34f888554322112403bea5226876
Joy_Newton <joynewton1964@gmail.com>
2021-05-13 12:07:40 +0530

    program updated

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,6 @@
 class Algo:
-    def sort(self, a , reverse = False):
+    def sort(self, a, reverse=False):
+        a = list(a)
         for i in range(len(a) - 1):
             for j in range(i, len(a) - 1):
                 if a[i] > a[j + 1]:
@@ -10,17 +11,17 @@ class Algo:
             a = a[::-1]
         return a
 
-    def max_value(self,x):
+    def max_value(self, x):
         # print(x)
-        for i in range(len(x)-1):
-            if x[i] > x[i + 1]:
-                temp = x[i]
-                x[i] = x[i + 1]
-                x[i + 1] = temp
+        max_value = self.sort(x)
+        return max_value[-1]
 
-        return x[-1]
+    def min_value(self, a):
+        a = list(a)
+        min_value = self.min_value(a)
+        return min_value[0]
 
-    def binary(self,decimal):
+    def binary(self, decimal):
         bin_number = []
         while decimal > 0:
             r = decimal % 2
@@ -28,19 +29,3 @@ class Algo:
             bin_number.append(r)
         bin_number.reverse()
         return bin_number
-
-    def min_value(self,a , maxvalue = False):
-        a = list(a)
-        for j in range(len(a) - 1):
-            for i in range(len(a) - 1):
-                if a[i] > a[i + 1]:
-                    temp = a[i]
-                    a[i] = a[i + 1]
-                    a[i + 1] = temp
-        if maxvalue :
-            return a[-1]
-        else:
-            return a[0]
-
-
-
