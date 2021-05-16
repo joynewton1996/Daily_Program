3821e1212e0b3413bdf6f76e9357a201f62c8eb6
Joy_Newton <joynewton1964@gmail.com>
2021-05-16 22:57:13 +0530

    return statement added

--- hacker_Rank/May16_Answer.py
+++ hacker_Rank/May16_Answer.py
@@ -1,12 +1,11 @@
 def solution(a):
     east = [car for car in range(len(a)) if a[car] == 0]
-    print(east)
     west = [car for car in range(len(a)) if a[car] == 1]
-    print(west)
-    total = [(e, w) for e in east for w in west]
-    print(total)
+    total = [(e, w) for e in east for w in west if w > e]
+    return len(total)
 
 
 if __name__ == "__main__":
     a = [0, 1, 0, 1, 1]
     result = solution(a)
+    print(result)
