d33c8a685ebc14a1bb99d9cf2c5c9f35438728d0
Joy_Newton <joynewton1964@gmail.com>
2021-05-16 17:29:52 +0530

    Sum modified

--- hacker_Rank/May14_Answer.py
+++ hacker_Rank/May14_Answer.py
@@ -1,15 +1,11 @@
-from algorithm import Algo
-
-obj = Algo()
-
-
 def solution(a):
     n = len(a)
+    minvalue_list = 0
     pairs = [(p, q) for p in range(n) for q in range(p + 1, n - 1)]
     minvalue = sum(a[0:1])
     for p, q in pairs:
-        Sum = sum(a[p:q + 1])
-        avg = Sum / (q - p + 1)
+        sum_pairs = sum(a[p:q + 1])
+        avg = sum_pairs / (q - p + 1)
         if avg < minvalue:
             minvalue = avg
             minvalue_list = p
