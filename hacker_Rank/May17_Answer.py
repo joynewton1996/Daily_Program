db290b2682bd62a1ee8e70a2b29332432da78e99
Joy_Newton <joynewton1964@gmail.com>
2021-05-17 21:47:35 +0530

    set done

--- hacker_Rank/May17_Answer.py
+++ hacker_Rank/May17_Answer.py
@@ -1,13 +1,9 @@
 def solution(A):
-    non_repeat = []
-    for num in a:
-        if num not in non_repeat:
-            non_repeat.append(num)
-    return len(non_repeat)
+    non_repeat = set(a)
+    return (len(non_repeat))
 
 
-
-if __name__=='__main__':
-    a = [2,1,1,2,3,1]
+if __name__ == '__main__':
+    a = [2, 1, 1, 2, 3, 1]
     result = solution(a)
     print(result)
