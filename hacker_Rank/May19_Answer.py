84e390ec56a21775ade4d36d42b117eaa3ec98f1
Joy_Newton <joynewton1964@gmail.com>
2021-05-24 12:15:33 +0530

    set added

--- hacker_Rank/May19_Answer.py
+++ hacker_Rank/May19_Answer.py
@@ -2,16 +2,14 @@ def solution(a):
     count = 0
     n = len(a)
     for j in range(n):
-        list_j = [j for j in range((-a[j] + j), (a[j] + j + 1))]
-        for k in range(j+1,n):
-            list_k = [k for k in range((-a[k] + k), (a[k] + k + 1))]
+        list_j = set(range((-a[j] + j), (a[j] + j + 1)))
+        for k in range(j + 1, n):
+            list_k = set(range((-a[k] + k), (a[k] + k + 1)))
             if set(list_j).intersection(list_k):
-                print(f"disc {j} intersect disk {k}")
-
                 count += 1
 
-
     return count
+
 
 if __name__ == "__main__":
     a = [1, 5, 2, 1, 4, 0]
