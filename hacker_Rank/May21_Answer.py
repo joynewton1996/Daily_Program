e1dc172f4eb09e34b7422bbffffa043d523eda1d
Joy_Newton <joynewton1964@gmail.com>
2021-05-24 12:21:58 +0530

    empty list short added

--- hacker_Rank/May21_Answer.py
+++ hacker_Rank/May21_Answer.py
@@ -5,7 +5,7 @@ def solution(a):
     n = len(a)
     triplet = [(p, q, r) for p, q, r in (product(range(n), repeat=3))
                if 0 <= p < q < r < n and a[p] + a[q] > a[r] and a[q] + a[r] > a[p] and a[r] + a[p] > a[q]]
-    if triplet > []:
+    if triplet:
         return 1
     else:
         return 0
