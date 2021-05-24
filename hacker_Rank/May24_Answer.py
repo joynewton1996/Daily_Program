d7588af1d2ad8a816a124a03adb92e5c52cce857
Joy_Newton <joynewton1964@gmail.com>
2021-05-24 20:44:41 +0530

    nested list program updated

---
+++ hacker_Rank/May24_Answer.py
@@ -0,0 +1,18 @@
+def solution(strings):
+    my_dict = {"(": ")"}
+    a = []
+    for item in strings:
+        if item in my_dict.keys():
+            a.append(item)
+        elif item in my_dict.values() and item == my_dict[a.pop()]:
+            pass
+    if not a:
+        return 1
+    else:
+        return 0
+
+
+if __name__ == "__main__":
+    string = "(()(())())"
+    result = solution(string)
+    print(result)
