59849237898256d2da30ecaa3530905fc30bf593
Joy_Newton <joynewton1964@gmail.com>
2021-05-26 01:07:27 +0530

    fish list are updated

--- hacker_Rank/May23_Answer.py
+++ hacker_Rank/May23_Answer.py
@@ -2,22 +2,44 @@ def solution(a, b):
     pairs = {key[i]: values[i] for i in range(len(a))}
     print(pairs)
     fish = list(a)
-    start = b.index(1)
+    fish_start = a[b.index(1)]
+    start = b.index(1) + 1
+    print(fish_start)
     end = len(a)
-    max_fish = a[start]
-
     for i in range(start, end):
-        if fish[start] > a[i]:
-            fish.pop(a[i])
-        elif fish[start] < a[i]:
-            fish.pop(fish[start])
-            max_fish = a[i]
-    print(fish)
+        print(f"iteration {i}")
+        if b[i] == 0:
+
+            if fish_start > a[i]:
+                fish.remove(a[i])
+
+            elif fish_start < a[i]:
+                fish.remove(fish_start)
+                fish_start = a[i]
+
+        else:
+            fish_start = a[i]
+
+    print("fish left in 1st loop", fish)
+    for j in range(len(fish) - 1):
+        if fish[j] > fish[j + 1]:
+            fish.remove(fish[j + 1])
+
+        elif fish[j] < fish[j - 1]:
+            fish.remove(fish[j])
+            fish[j] = a[j - 1]
+    print("fish left in the 2nd loop", fish)
     return (len(fish))
 
 
 if __name__ == "__main__":
     key = [4, 3, 2, 1, 5]
     values = [0, 1, 0, 0, 0]
-    result = solution(key, values)
+    a = [6, 3, 2, 1, 5]
+    b = [1, 0, 0, 0, 0]
+    c = [4, 3, 2, 1, 5]
+    d = [1, 1, 0, 0, 0]
+    e = [6, 3, 2, 1, 5]
+    f = [1, 1, 0, 0, 0]
+    result = solution(c,d )
     print(result)
