a9d608b77b355c08e2ffdbfae492d44ebe7e60c4
Joy_Newton <joynewton1964@gmail.com>
2021-05-31 13:23:17 +0530

    changes applied

--- hacker_Rank/May27_Answer.py
+++ hacker_Rank/May27_Answer.py
@@ -3,16 +3,13 @@ def solution(a):
     position = 0
     for i in range(1, len(a)):
         c = a[:i]
+        c_len = len(c) / 2
         d = a[i:]
-
-        if len(c) >= 1 and len(d) >= 1:
-
-            count_c = {l: c.count(l) for l in c}
-            count_d = {k: d.count(k) for k in d}
-            max_count_c = max(count_c.values())
-            max_count_d = max(count_d.values())
-            if max_count_d == max_count_c and max_count_d > len(d) / 2 and max_count_c > len(c) / 2:
-                position += 1
+        d_len = len(d) / 2
+        count_c = [i for i in c if c.count(i) > c_len and i == max(c)]
+        count_d = [j for j in d if d.count(j) > d_len and j == max(d)]
+        if set(count_c) == set(count_d):
+            position += 1
 
     if position:
         return position
