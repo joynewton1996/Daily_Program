e2e887b7e50f1a8331794e4004b8877c3b82b3a6
Joy_Newton <joynewton1964@gmail.com>
2021-06-02 21:19:38 +0530

    max slice number completed

---
+++ hacker_Rank/June2_Answer.py
@@ -0,0 +1,19 @@
+def solution(a):
+    n = len(a)
+    try:
+        slice_sum = max([sum(a[p:q]) for p in range(len(a)) for q in range(len(a)) if 0 <= p <= q <= n])
+
+    except IndexError:
+        print("Index_Error caught")
+    else:
+
+        yield f"slice_sum of the list is : {slice_sum}"
+    finally:
+        print("Your work completed")
+
+
+
+if __name__ == "__main__":
+    a = [3, 2, -6, 4, 0]
+    result = solution(a)
+    print(next(result))
