0d5c8d84ad61d18bc5ab842f37def41fcdf8d504
joynewton1996 <82137922+joynewton1996@users.noreply.github.com>
2021-06-04 12:23:25 +0530

    Trading program upated (#49)

---
+++ hacker_Rank/June1_Answer.py
@@ -0,0 +1,25 @@
+def solution(a):
+    a = list(a)
+    n = len(a)
+    try:
+        stock_price = max(
+            [a[sell] - a[buy] for buy in range(n) for sell in range(n) if 0 <= buy < sell if (a[sell] - a[buy]) > 0])
+
+
+
+    except ValueError:
+        return 0
+    except IndexError:
+        return "Index Error"
+    else:
+        return stock_price
+
+    finally:
+        print("Thanks for trading")
+
+
+if __name__ == "__main__":
+    a = [23171, 21011, 21123, 21366, 21013, 21367]
+    b = (23, 21, 18)
+    result = solution(a)
+    print(result)
