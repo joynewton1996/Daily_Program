a568256aa763e48926f3f7a06c6567aee756b72e
Joy_Newton <joynewton1964@gmail.com>
2021-06-05 21:25:34 +0530

    flag problem updated

--- hacker_Rank/June4_Answer.py
+++ hacker_Rank/June4_Answer.py
@@ -1,23 +1,16 @@
-def solution(a, k_flag):
+def solution(a):
     n = len(a)
-    a_dict = {p: a[p] for p in range(n) if 0 < p < n - 1 if a[p - 1] < a[p] > a[p + 1]}
-    peak_max = sorted(a_dict.values())
-    flag_point = peak_max[-k_flag:]
     try:
-
-        if k_flag <= len(flag_point):
-            flag = [key for flag in flag_point for key in a_dict if a_dict[key] == flag]
-            return sorted(flag[-k_flag:])
+        peak_max = set(sorted([a[p] for p in range(n) if 0 < p < n - 1 if a[p - 1] < a[p] > a[p + 1]]))
     except IndexError:
-        print("Index error")
+        return ("Index error")
     else:
-        print(f"maximum flag required is {len(flag_point)} " )
+        return (f"maximum flag required is {len(peak_max)} ")
     finally:
         print("Thanks for climbling the hill")
 
 
 if __name__ == "__main__":
     a = [1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2]
-    k_flag = 3
-    result = solution(a, k_flag)
+    result = solution(a)
     print(result)
