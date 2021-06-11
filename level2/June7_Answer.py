a81cbea2aa36300b849ed333ebf4b0e4ffcf1064
Joy_Newton <joynewton1964@gmail.com>
2021-06-11 10:08:38 +0530

    updated

--- level2/June7_Answer.py
+++ level2/June7_Answer.py
@@ -1,8 +1,22 @@
-def solution(string):
-    sub = set([ character for character in string])
+# Method 1
+def solution1(string):
+    sub = set(string)
     return len(sub)
+
+# Method 2
+def solution2(string):
+    j = ""
+    for i in string:
+        if i not in j:
+            j = j+i
+    return len(j)
+
+
+
 
 if __name__=="__main__":
     string =  "abcabcbb"
-    result = solution(string)
-    print(result)
+    result1 = solution1(string)
+    result2 = solution2(string)
+    print(result1)
+    print(result2)
